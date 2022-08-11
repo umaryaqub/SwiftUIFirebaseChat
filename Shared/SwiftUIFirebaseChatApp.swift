a2777ab56fcebf0f2fabd6a7f0e1ef2a5d0ccf98
Umar Yaqub <yaqub_umar@yahoo.co.uk>
2022-08-11 01:19:15 +0100

    Initial Commit

---
+++ Shared/SwiftUIFirebaseChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIFirebaseChatApp.swift
+//  Shared
+//
+//  Created by Mishu on 11/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIFirebaseChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
